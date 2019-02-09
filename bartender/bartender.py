a02d996c026eab1ee7901c934c5f1673e0247215
Stephen Ritchie <stephenfritchie@gmail.com>
2019-02-09 15:41:22 -0500

    test update

---
+++ bartender/bartender.py
@@ -0,0 +1,34 @@
+
+
+# Functions I need to make
+# Public
+# make_drink(name)
+# clean()
+#
+# Private
+# load_drink_list()
+# load_pump_config()
+# record_drink(name, person?)
+# pour(pin, time)
+
+import json
+from pprint import pprint
+
+
+class Bartender:
+
+	def __init__(self):
+		self.load_drink_list()
+		
+
+	def load_drink_list(self):
+		filename = "drinklist.json"
+		with open(filename) as f:
+			data = json.load(f)
+		pprint(data)
+
+
+
+
+
+app = Bartender()
